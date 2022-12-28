82cb35323457e5cbb237558487a779c7ff8447a1
paulagaleano <paulagaleano@live.com.ar>
2022-12-28 01:57:17 -0300

    node

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express();
+
+//Setear todo el servidor
+
+//Endpoints (Todos los request que recibe mi servidor y contesta + "get" = tipo de mensaje http)
+
+app.get("/", (req, res) => {  //ENDPOINT BASE
+    res.send("Hola mundo");
+});
+
+app.get("/bienvenido", function (req, res) {
+    const respuesta = `
+    <html> 
+        <body> 
+            <h1> la concha de tu tia </h1>
+        </body>
+    </html>
+    `;
+    res.send(respuesta);
+});
+
+app.get('/usuario', (req, res) => {
+    const us = {
+        nombre: 'Paula',
+        apellido: 'G',
+        edad: 100,
+        mail: 'ppaug@gmail.com'
+    }
+    res.send(us);
+});
+
+//OBJETO REQUEST
+
+
+
+//Levanto el servidor
+app.listen(3000);
